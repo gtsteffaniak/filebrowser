1429963e76e1d675ae61487d4d78b409c7f4b0ec
Graham Steffaniak <42989099+gtsteffaniak@users.noreply.github.com>
2023-10-10 10:03:03 -0500

    updated test (#44)
    Co-authored-by: Graham Steffaniak <graham.steffaniak@autodesk.com>

--- backend/settings/config_test.go
+++ backend/settings/config_test.go
@@ -1,56 +1,9 @@
 package settings
 
 import (
-	"log"
 	"reflect"
 	"testing"
-
-	"github.com/goccy/go-yaml"
-	"github.com/google/go-cmp/cmp"
 )
-
-func TestConfigLoadChanged(t *testing.T) {
-	yamlData := loadConfigFile("./testingConfig.yaml")
-	// Marshal the YAML data to a more human-readable format
-	newConfig := setDefaults()
-	GlobalConfiguration := setDefaults()
-
-	err := yaml.Unmarshal(yamlData, &newConfig)
-	if err != nil {
-		log.Fatalf("Error unmarshaling YAML data: %v", err)
-	}
-	// Use go-cmp to compare the two structs
-	if diff := cmp.Diff(newConfig, GlobalConfiguration); diff == "" {
-		t.Errorf("No change when there should have been (-want +got):\n%s", diff)
-	}
-}
-
-func TestConfigLoadSpecificValues(t *testing.T) {
-	yamlData := loadConfigFile("./testingConfig.yaml")
-	// Marshal the YAML data to a more human-readable format
-	newConfig := setDefaults()
-	GlobalConfiguration := setDefaults()
-
-	err := yaml.Unmarshal(yamlData, &newConfig)
-	if err != nil {
-		log.Fatalf("Error unmarshaling YAML data: %v", err)
-	}
-	testCases := []struct {
-		fieldName string
-		globalVal interface{}
-		newVal    interface{}
-	}{
-		{"Auth.Method", GlobalConfiguration.Auth.Method, newConfig.Auth.Method},
-		{"UserDefaults.HideDotfiles", GlobalConfiguration.UserDefaults.HideDotfiles, newConfig.UserDefaults.HideDotfiles},
-		{"Server.Database", GlobalConfiguration.Server.Database, newConfig.Server.Database},
-	}
-
-	for _, tc := range testCases {
-		if tc.globalVal == tc.newVal {
-			t.Errorf("Differences should have been found:\n\tGlobalConfig.%s: %v \n\tSetConfig: %v \n", tc.fieldName, tc.globalVal, tc.newVal)
-		}
-	}
-}
 
 func TestInitialize(t *testing.T) {
 	type args struct {
