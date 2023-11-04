df83cee737f670776e8ca778db904f5e1b82ec0e
Graham Steffaniak <42989099+gtsteffaniak@users.noreply.github.com>
2023-11-04 10:01:09 -0500

    Fix index io (#67)
    Co-authored-by: Graham Steffaniak <graham.steffaniak@autodesk.com>

--- backend/index/indexing.go
+++ backend/index/indexing.go
@@ -1,6 +1,7 @@
 package index
 
 import (
+	"bytes"
 	"log"
 	"os"
 	"strings"
@@ -65,10 +66,12 @@ func indexingScheduler(intervalMinutes uint32) {
 		if index.currentlyIndexing {
 			continue
 		}
+		index.currentlyIndexing = true
 		err := index.indexFiles(index.Root)
 		if err != nil {
 			log.Printf("Error during indexing: %v", err)
 		}
+		index.currentlyIndexing = false
 		index.LastIndexed = time.Now()
 		if index.NumFiles+index.NumDirs > 0 {
 			timeIndexedInSeconds := int(time.Since(startTime).Seconds())
@@ -83,51 +86,51 @@ func indexingScheduler(intervalMinutes uint32) {
 
 // Define a function to recursively index files and directories
 func (si *Index) indexFiles(path string) error {
-	si.currentlyIndexing = true
 	// Check if the current directory has been modified since last indexing
 	path = strings.TrimSuffix(path, "/")
 	dir, err := os.Open(path)
 	if err != nil {
-		// Directory must have been deleted, remove from index
+		// Directory must have been deleted, remove from the index
 		si.Directories = removeFromSlice(si.Directories, path)
 	}
 	defer dir.Close()
 	dirInfo, err := dir.Stat()
 	if err != nil {
-		si.currentlyIndexing = false
 		return err
 	}
 	// Compare the last modified time of the directory with the last indexed time
 	if dirInfo.ModTime().Before(lastIndexed) {
-		si.currentlyIndexing = false
 		return nil
 	}
 	// Read the directory contents
 	files, err := dir.Readdir(-1)
 	if err != nil {
-		si.currentlyIndexing = false
 		return err
 	}
 	adjustedPath := strings.TrimPrefix(path, si.Root+"/")
 	adjustedPath = strings.TrimSuffix(adjustedPath, "/")
 	keyVal := -1
-	//exists := false
-	for k, v := range index.Directories {
+	// Find the key for the directory
+	for k, v := range si.Directories {
 		if v.Name == adjustedPath {
-			//exists := true
 			keyVal = k
-			continue
+			break
 		}
 	}
+	// Create a buffer for the directory
+	var buffer bytes.Buffer
 	// Iterate over the files and directories
 	for _, file := range files {
-		si.Insert(path, file.Name(), file.IsDir(), keyVal)
+		si.Insert(path, file.Name(), file.IsDir(), keyVal, &buffer)
 	}
-	si.currentlyIndexing = false
+	if keyVal != -1 {
+		// Store the buffer in the directory's Files field
+		si.Directories[keyVal].Files = buffer.String()
+	}
 	return nil
 }
 
-func (si *Index) Insert(path string, fileName string, isDir bool, key int) {
+func (si *Index) Insert(path string, fileName string, isDir bool, key int, buffer *bytes.Buffer) {
 	adjustedPath := strings.TrimPrefix(path, si.Root+"/")
 	adjustedPath = strings.TrimSuffix(adjustedPath, "/")
 	// Check if it's a directory or a file
@@ -137,19 +140,19 @@ func (si *Index) Insert(path string, fileName string, isDir bool, key int) {
 			Name: adjustedPath + "/" + fileName,
 		} // 48
 		si.mutex.Lock()
-		index.Directories = append(index.Directories, subDirectory)
+		si.Directories = append(index.Directories, subDirectory)
 		si.mutex.Unlock()
 
 		// Recursively index the directory
-		err := index.indexFiles(path + "/" + fileName)
+		err := si.indexFiles(path + "/" + fileName)
 		if err != nil {
 			log.Printf("Could not index \"%v\": %v \n", path, err)
 		}
 	} else {
 		if key != -1 {
-			si.mutex.Lock()
-			index.Directories[key].Files += fileName + ";"
-			si.mutex.Unlock()
+			// Use the buffer for this directory to concatenate file names
+			buffer.WriteString(fileName)
+			buffer.WriteString(";")
 			si.NumFiles++
 		}
 	}
