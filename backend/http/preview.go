2ad0542e345d27fc9102c20195a68565b576b8ca
Graham Steffaniak <42989099+gtsteffaniak@users.noreply.github.com>
2023-12-01 14:41:34 -0600

    Revert "Update file reading (#79)"
    This reverts commit 42c1c6222f0528d250e511cfa6dfb222ae01817e.

--- backend/http/preview.go
+++ backend/http/preview.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -53,6 +54,8 @@ func previewHandler(imgSvc ImgService, fileCache FileCache, enableThumbnails, re
 			ReadHeader: d.server.TypeDetectionByHeader,
 			Checker:    d,
 		})
+		log.Println("updated", file.Type, file.Name)
+
 		if err != nil {
 			return errToStatus(err), err
 		}
@@ -76,6 +79,8 @@ func handleImagePreview(
 	previewSize PreviewSize,
 	enableThumbnails, resizePreview bool,
 ) (int, error) {
+	log.Println("handle preview ", file.Name)
+
 	if (previewSize == PreviewSizeBig && !resizePreview) ||
 		(previewSize == PreviewSizeThumb && !enableThumbnails) {
 		return rawFileHandler(w, r, file)
