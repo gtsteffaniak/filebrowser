42565101179585a2030c012f098fecee5fb2fd57
Graham Steffaniak <gtsteff@gmail.com>
2024-09-02 19:13:48 -0500

    updated linting

--- frontend/src/views/files/ListingView.vue
+++ frontend/src/views/files/ListingView.vue
@@ -472,6 +472,8 @@ export default {
       if (getters.currentPromptName() != null) {
         return;
       }
+      let currentPath = state.route.path.replace(/\/+$/, ""); // Remove trailing slashes
+      let newPath = currentPath.substring(0, currentPath.lastIndexOf("/"));
       // Handle key events using a switch statement
       switch (key) {
         case "Enter":
@@ -482,8 +484,6 @@ export default {
 
         case "Backspace":
           // go back
-          let currentPath = state.route.path.replace(/\/+$/, ""); // Remove trailing slashes
-          let newPath = currentPath.substring(0, currentPath.lastIndexOf("/"));
           router.push({ path: newPath });
           break;
 
