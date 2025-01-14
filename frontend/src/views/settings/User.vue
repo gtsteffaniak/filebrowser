c0cc07c6881e722ae08f846513d7b9991600354f
Graham Steffaniak <graham.steffaniak@autodesk.com>
2025-01-13 19:29:58 -0600

    updated tests

--- frontend/src/views/settings/User.vue
+++ frontend/src/views/settings/User.vue
@@ -110,7 +110,7 @@ export default {
       event.preventDefault();
       try {
         if (this.isNew) {
-          const loc = await usersApi.create(this.userPayload); // Use the computed property
+          await usersApi.create(this.userPayload); // Use the computed property
           this.$router.push({ path: "/settings", hash: "#users-main" });
           notify.showSuccess(this.$t("settings.userCreated"));
         } else {
