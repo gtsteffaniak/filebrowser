a5559ec3e2a5254fcdaf0664180e1dbc21329e08
Graham Steffaniak <42989099+gtsteffaniak@users.noreply.github.com>
2025-04-09 14:01:35 -0500

    updated with autobackup (#514)

--- backend/cmd/user.go
+++ backend/cmd/user.go
@@ -3,10 +3,13 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/gtsteffaniak/filebrowser/backend/adapters/fs/fileutils"
 	"github.com/gtsteffaniak/filebrowser/backend/common/logger"
 	"github.com/gtsteffaniak/filebrowser/backend/common/settings"
 	"github.com/gtsteffaniak/filebrowser/backend/database/users"
 )
+
+var createBackup = []bool{}
 
 func validateUserInfo() {
 	// update source info for users if names/sources/paths might have changed
@@ -27,6 +30,13 @@ func validateUserInfo() {
 			updateUser = true
 		}
 		if updateUser {
+			if len(createBackup) == 1 {
+				logger.Warning("Incompatible user settings detected, creating backup of database before converting.")
+				err = fileutils.CopyFile(settings.Config.Server.Database, fmt.Sprintf("%s.bak", settings.Config.Server.Database))
+				if err != nil {
+					logger.Fatal(fmt.Sprintf("Unable to create automatic backup of database due to error: %v", err))
+				}
+			}
 			err := store.Users.Save(user, false)
 			if err != nil {
 				logger.Error(fmt.Sprintf("could not update user: %v", err))
@@ -80,19 +90,26 @@ func updatePermissions(user *users.User) bool {
 	// if any keys are true, set the permissions to true
 	if user.Perm.Api {
 		user.Permissions.Api = true
+		user.Perm.Api = false
 		updateUser = true
 	}
 	if user.Perm.Admin {
 		user.Permissions.Admin = true
+		user.Perm.Admin = false
 		updateUser = true
 	}
 	if user.Perm.Modify {
 		user.Permissions.Modify = true
+		user.Perm.Modify = false
 		updateUser = true
 	}
 	if user.Perm.Share {
 		user.Permissions.Share = true
+		user.Perm.Share = false
 		updateUser = true
+	}
+	if updateUser {
+		createBackup = append(createBackup, true)
 	}
 	return updateUser
 }
