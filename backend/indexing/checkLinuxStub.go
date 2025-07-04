0c70b1d5103344ff46479cc5f5c5bb1b5efe3191
Graham Steffaniak <gtsteff@gmail.com>
2025-07-04 17:37:50 -0500

    added windows partition size support

--- backend/indexing/checkLinuxStub.go
+++ backend/indexing/checkLinuxStub.go
@@ -3,7 +3,20 @@
 
 package indexing
 
+import "syscall"
+
 func CheckWindowsHidden(realpath string) bool {
 	// Non-Windows platforms don't support hidden attributes in the same way
 	return false
+}
+
+func getPartitionSize(path string) (uint64, error) {
+	var stat syscall.Statfs_t
+	err := syscall.Statfs(path, &stat)
+	if err != nil {
+		return 0, err
+	}
+	// Total size in bytes: Blocks * Block size
+	total := stat.Blocks * uint64(stat.Bsize)
+	return total, nil
 }
