09e236fb94817d1739beeb4c41aa1884ae5de633
Kurami <178341547+Kurami32@users.noreply.github.com>
2025-08-28 14:16:26 -0500

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,8 @@ import App from './App.vue'; // Adjust the path as per your setup
 import { state } from '@/store'; // Adjust the path as per your setup
 import i18n from "@/i18n";
 import VueLazyload from "vue-lazyload";
+import VuePlyr from "@skjnldsv/vue-plyr"; // Custom media player
+import "@skjnldsv/vue-plyr/dist/vue-plyr.css";
 
 import './css/styles.css';
 
