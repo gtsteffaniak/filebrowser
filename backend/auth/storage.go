b8f69b7fece92a56b5d264a683752eb08f0fc77a
Graham Steffaniak <graham.steffaniak@autodesk.com>
2025-10-01 16:29:19 -0500

    updated

--- backend/auth/storage.go
+++ backend/auth/storage.go
@@ -2,12 +2,12 @@ package auth
 
 import (
 	"encoding/base64"
-	"log"
 
 	"crypto/sha256"
 
 	"github.com/gtsteffaniak/filebrowser/backend/common/settings"
 	"github.com/gtsteffaniak/filebrowser/backend/database/users"
+	"github.com/gtsteffaniak/go-logger/logger"
 	"golang.org/x/crypto/pbkdf2"
 )
 
@@ -42,12 +42,12 @@ func NewStorage(back StorageBackend, userStore *users.Storage) (*Storage, error)
 		key, err := base64.StdEncoding.DecodeString(settings.Config.Auth.TotpSecret)
 		keyLen := len(key)
 
-		if err == nil && (keyLen == 16 || keyLen == 24 || keyLen == 25) {
+		if err == nil && (keyLen == 16 || keyLen == 24 || keyLen == 32) {
 			// Use the user-provided key if it's valid
 			encryptionKey = key
 		} else {
 			// Otherwise, derive the key from the provided secret string
-			log.Println("[WARN] totpSecret is not a valid Base64 encoded key. Deriving a key from it. For better security, generate a secret with 'openssl rand -base64 32'.")
+			logger.Warningf("totpSecret is not a valid Base64 encoded key. Deriving a key from it. For better security, generate a secret with 'openssl rand -base64 32'.")
 			salt := []byte{0xda, 0x90, 0x45, 0xc3, 0x06, 0xb5, 0x99, 0x9f, 0xb6, 0xae, 0xfc, 0x14, 0xef, 0x27, 0x6e, 0x6a}
 			encryptionKey = pbkdf2.Key([]byte(settings.Config.Auth.TotpSecret), salt, 4096, 32, sha256.New)
 		}
