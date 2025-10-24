cb71484a872062f2b68fabec53634e190a30dcb1
Graham Steffaniak <42989099+gtsteffaniak@users.noreply.github.com>
2025-10-24 11:47:29 -0500

    proxyAuth to fetch auth token (#1429)

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -25,7 +25,6 @@ export async function validateLogin() {
   const userInfo = await res.json();
   mutations.setCurrentUser(userInfo);
   getters.isLoggedIn()
-
   if (state.user.loginMethod == "proxy") {
     let apiPath = getApiPath("api/auth/login")
     const res = await fetch(apiPath, {
