0c23cc83249ddd8e90b2c1f0e013beb02bfb8ace
Graham Steffaniak <42989099+gtsteffaniak@users.noreply.github.com>
2026-01-30 15:50:36 -0500

    updated with virtual scrolling (#1913)

--- frontend/tests/playwright/settings/access.spec.ts
+++ frontend/tests/playwright/settings/access.spec.ts
@@ -4,25 +4,31 @@ test("access rules - deny folder does not show in folder listing", async ({ page
   await page.goto("/files/access");
   await expect(page).toHaveTitle("Graham's Filebrowser - Files - playwright-files");
   // expect the excluded folder to not be visible
-  await expect(page.locator('div[aria-label="excluded"]')).toBeHidden();
+  await expect(page.locator('a[aria-label="excluded"]')).toBeHidden();
+  checkForErrors();
+});
+
+test("access rules - main denyByDefault root exception works", async ({ page, checkForErrors, context }) => {
+  await page.goto("/files/access");
+  await expect(page).toHaveTitle("Graham's Filebrowser - Files - playwright-files");
+  await expect(page.locator('a[aria-label="text-files"]')).toBeVisible();
   checkForErrors();
 });
 
 test("access rules - deny folder has access denied message", async ({ page, checkForErrors, context }) => {
-  await page.goto("/files/access");
-  await expect(page).toHaveTitle("Graham's Filebrowser - Files - playwright-files");
+  await page.goto("/files/access/excluded");
+  await expect(page).toHaveTitle("Graham's Filebrowser - Files");
   const msg = "403: access denied"
   await checkForNotification(page, msg);
   // expect the excluded folder to have an access denied message
-  checkForErrors(1,1); // expect 1 api error
+  checkForErrors(1,1);
 });
 
 test("navigate from search item", async({ page, checkForErrors, context }) => {
   await page.goto("/files/");
   await expect(page).toHaveTitle("Graham's Filebrowser - Files - playwright-files");
   await page.locator('#search').click()
-  await page.locator('#search-sources-dropdown').click()
-  await page.locator('#search-sources-dropdown option[value="access"]').click()
+  await page.locator('select[aria-label="search sources dropdown"]').selectOption('access');
   await page.locator('#main-input').fill('showme.txt');
   await expect(page.locator('#result-list')).toHaveCount(0);
   checkForErrors()
