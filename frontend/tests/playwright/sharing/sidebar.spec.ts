6b18ed36479b3c0a83d3938f52a5bc3a4a7781f9
Graham Steffaniak <graham.steffaniak@autodesk.com>
2026-03-05 16:05:45 -0600

    updated share

--- frontend/tests/playwright/sharing/sidebar.spec.ts
+++ frontend/tests/playwright/sharing/sidebar.spec.ts
@@ -77,7 +77,7 @@ test("edit previously created links and ensure they are updated", async ({ page,
     await page.goto("/public/share/" + shareHash);
     await expect(page).toHaveTitle("Graham's Filebrowser - Share - text-files");
     // sidebar should have 4 items (ShareInfo, Download, New Custom Link, Go to source location)
-    await expect(page.locator'.sidebar-links .inner-card'().locator('a')).toHaveCount(4);
+    await expect(page.locator('.sidebar-links .inner-card').locator('a')).toHaveCount(4);
     // check items exist
     await page.locator('a[aria-label="Share QR Code and Info"]').waitFor({ state: 'visible' });
     await page.locator('a[aria-label="Download"]').waitFor({ state: 'visible' });
