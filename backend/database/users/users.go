90f809ba85360a386a22230ff2c0033e7546ea62
SIDDHU SINGH <siddhu3116@gmail.com>
2026-04-11 02:14:25 +0530

    #2204 feat(context-menu): add copy path action (#2253)

--- backend/database/users/users.go
+++ backend/database/users/users.go
@@ -121,6 +121,7 @@ type NonAdminEditable struct {
 	DisableViewingExt          string        `json:"disableViewingExt"`          // space separated list of file extensions to disable viewing for
 	CustomTheme                string        `json:"customTheme"`                // Name of theme to use chosen from custom themes config.
 	ShowSelectMultiple         bool          `json:"showSelectMultiple"`         // show select multiple files on desktop
+	ShowCopyPath               bool          `json:"showCopyPath"`               // show copy path action in the context menu
 	DebugOffice                bool          `json:"debugOffice"`                // debug onlyoffice editor
 	OtpEnabled                 bool          `json:"otpEnabled"`                 // allow non-admin users to disable their own OTP
 	SidebarLinks               []SidebarLink `json:"sidebarLinks"`               // customizable sidebar links
