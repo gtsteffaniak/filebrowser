a453e4b52abf8b8c90f7ab843f9849ae84999de6
Graham Steffaniak <graham.steffaniak@autodesk.com>
2026-05-20 14:10:48 -0500

    updated access rules

--- frontend/tests/playwright/settings/access.spec.ts
+++ frontend/tests/playwright/settings/access.spec.ts
@@ -8,6 +8,20 @@ test("access rules - deny folder does not show in folder listing", async ({ page
   checkForErrors();
 });
 
+test("access rules - deny folder with child access-allowed item shows", async ({ page, checkForErrors, context }) => {
+  await page.goto("/files/access");
+  await expect(page).toHaveTitle("Graham's Filebrowser - Files - playwright-files");
+  // expect the excluded folder to not be visible (because its denied and rules only check direct child items)
+  await expect(page.locator('a[aria-label="excluded"]')).toBeHidden();
+
+  // go into folder to see items within it
+  await page.goto("/files/access/excluded");
+  await expect(page.locator('a[aria-label="no-access.txt"]')).toBeHidden();
+  await expect(page.locator('a[aria-label="showme.txt"]')).toBeVisible();
+
+  checkForErrors();
+});
+
 test("access rules - main denyByDefault root exception works", async ({ page, checkForErrors, context }) => {
   await page.goto("/files/access");
   await expect(page).toHaveTitle("Graham's Filebrowser - Files - playwright-files");
@@ -16,11 +30,11 @@ test("access rules - main denyByDefault root exception works", async ({ page, ch
 });
 
 test("access rules - deny folder has access denied message", async ({ page, checkForErrors, context }) => {
-  await page.goto("/files/access/excluded");
+  await page.goto("/files/access/denied");
   await expect(page).toHaveTitle("Graham's Filebrowser - Files");
   const msg = "403: access denied"
   await checkForNotification(page, msg);
-  // expect the excluded folder to have an access denied message
+  // expect the denied folder to have an access denied message
   checkForErrors(1,1);
 });
 
@@ -29,8 +43,10 @@ test("navigate from search item", async({ page, checkForErrors, context }) => {
   await expect(page).toHaveTitle("Graham's Filebrowser - Files - playwright-files");
   await page.locator('#search-bar-input').click()
   await page.locator('select[aria-label="search sources dropdown"]').selectOption('access');
-  await page.locator('#search-input').fill('showme.txt');
+  await page.locator('#search-input').fill('no-access.txt');
   await expect(page.locator('.searchPrompt p')).toHaveText('No results found.');
+  await page.locator('#search-input').fill('showme.txt');
+  await expect(page.locator('#result-list ul li.search-entry')).toHaveCount(1);
   checkForErrors()
 });
 
