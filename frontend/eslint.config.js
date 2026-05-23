1adf996e14011521dfb4484f5524f093e81d2d66
Kevin B. <kurami32_kbm@proton.me>
2026-05-23 07:43:10 -0500

    organize eslint config and enable recommended i18n rules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import pluginI18n from "@intlify/eslint-plugin-vue-i18n";
+import vueI18n from '@intlify/eslint-plugin-vue-i18n';
 import vueParser from "vue-eslint-parser";
 import globals from "globals";
 
@@ -19,9 +19,32 @@ export default defineConfig(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
+  ...vueI18n.configs.recommended,
+
+  // i18n
+  {
+    settings: {
+      "vue-i18n": {
+        localeDir: "src/i18n/en.json",
+        messageSyntaxVersion: "^9.0.0", // For now this on v9, we can change it updating vue-i18n to v11
+      },
+    },
+    rules: {
+      "@intlify/vue-i18n/no-missing-keys": "error",
+      "@intlify/vue-i18n/no-unused-keys": ["error", {
+        src: "./src",
+        extensions: [".js", ".vue", ".ts"],
+      }],
+      "@intlify/vue-i18n/no-raw-text": ["error", {
+        ignoreNodes: ["i", "v-icon"],
+      }],
+      "@intlify/vue-i18n/no-missing-keys-in-other-locales": "warn",
+    },
+  },
 
   // Shared globals + rule overrides
   {
+    files: ["**/*.js", "**/*.ts", "**/*.vue"],
     languageOptions: {
       globals: {
         ...globals.node,
@@ -43,8 +66,6 @@ export default defineConfig(
         varsIgnorePattern: "^_",
         caughtErrors: "none",
       }],
-      "vue/no-reserved-component-names": "off",
-      "vue/no-unused-components": "warn",
       "eqeqeq": ["warn", "always"],
       "no-var": "error",
       "prefer-const": "warn",
@@ -63,7 +84,7 @@ export default defineConfig(
   },
 
   {
-    files: ["**/*.ts", "**/*.js" ],
+    files: ["**/*.ts", "**/*.js"],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
@@ -72,31 +93,6 @@ export default defineConfig(
         tsconfigRootDir: import.meta.dirname,
         extraFileExtensions: ['.vue'],
       },
-    },
-  },
-
-  // i18n
-  {
-    files: ["**/*.vue", "**/*.js", "**/*.ts"],
-    plugins: {
-      "@intlify/vue-i18n": pluginI18n,
-    },
-    settings: {
-      "vue-i18n": {
-        localeDir: "src/i18n/en.json",
-        messageSyntaxVersion: "^9.0.0",
-      },
-    },
-    rules: {
-      "@intlify/vue-i18n/no-missing-keys": "error",
-      "@intlify/vue-i18n/no-unused-keys": ["error", {
-        src: "./src",
-        extensions: [".js", ".vue", ".ts"],
-      }],
-      "@intlify/vue-i18n/no-raw-text": ["error", {
-        ignoreNodes: ["i", "v-icon"],
-      }],
-      "@intlify/vue-i18n/no-missing-keys-in-other-locales": "warn",
     },
   },
 
@@ -118,6 +114,8 @@ export default defineConfig(
       "vue/no-mutating-props": ["error", { shallowOnly: true }],
       //"vue/order-in-components": "warn",
       "vue/require-v-for-key": "error",
+      "vue/no-reserved-component-names": "off",
+      "vue/no-unused-components": "warn",
     },
   },
 );
