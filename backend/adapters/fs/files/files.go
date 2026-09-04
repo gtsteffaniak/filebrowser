5d9b4df2a21d1ba4a6af481181a7402cb5cbb5ca
Graham Steffaniak <42989099+gtsteffaniak@users.noreply.github.com>
2026-09-04 16:27:23 -0500

    fix GHSA-55mw-cwg7-m8f5 (#2904)

--- backend/adapters/fs/files/files.go
+++ backend/adapters/fs/files/files.go
@@ -382,7 +382,7 @@ func processContent(info *iteminfo.ExtendedFileInfo, idx *indexing.Index, opts u
 	}
 
 	// Process text content for non-video, non-audio files
-	if info.Size < 20*1024*1024 { // 20 megabytes in bytes
+	if opts.Content && info.Size < 20*1024*1024 { // 20 megabytes in bytes
 		content, err := getContent(info.RealPath)
 		if err != nil {
 			logger.Debugf("could not get content for file: "+info.RealPath, info.Name, err)
